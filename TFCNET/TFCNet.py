07a1ffc3dbedcdc76ad57c4187333ad022abad6f
xiaodong zhu <72681553+zxd3099@users.noreply.github.com>
2023-07-12 10:54:58 +0800

    Update TFCNet.py

--- TFCNET/TFCNet.py
+++ TFCNET/TFCNet.py
@@ -8,24 +8,35 @@
 import torch
 import torch.nn as nn
 from corrector import Corrector
+from MSF import MultiScaleBlock
 
 
 class TFCNet(nn.Module):
     def __init__(self,
                  n_fft: int = 512,
-                 hop_length: int = 16,
-                 window_length: int = 64):
+                 hop_length1: int = 16,
+                 window_length1: int = 64,
+                 hop_length2: int = 256,
+                 window_length2: int = 1024):
         super(TFCNet, self).__init__()
         self.corrector = Corrector()
+        self.msf = MultiScaleBlock()
+
         self.n_fft = n_fft
-        self.hop_length = hop_length
-        self.window_length = window_length
+        self.hop_length1 = hop_length1
+        self.window_length1 = window_length1
+        self.hop_length2 = hop_length2
+        self.window_length2 = window_length2
 
     def forward(self, x, x1, x2):
         # x : [B, L]；t : [B, F, T, 2]
-        t = torch.stft(x, self.n_fft, hop_length=self.hop_length, win_length=self.window_length)
-        t1 = torch.stft(x1, self.n_fft, hop_length=self.hop_length, win_length=self.window_length)
-        t2 = torch.stft(x2, self.n_fft, hop_length=self.hop_length, win_length=self.window_length)
+        t = torch.stft(x, self.n_fft, hop_length=self.hop_length1, win_length=self.window_length1)
+        t_large = torch.stft(x, self.n_fft, hop_length=self.hop_length2, win_length=self.window_length2)
+        t1 = torch.stft(x1, self.n_fft, hop_length=self.hop_length1, win_length=self.window_length1)
+        t2 = torch.stft(x2, self.n_fft, hop_length=self.hop_length1, win_length=self.window_length1)
+
+        # MSF
+        t = self.msf(t, t_large)
 
         # input : [B, F, T, 6], output: [B, F, T, 4]
         input = torch.cat([t, t1, t2], -1)
